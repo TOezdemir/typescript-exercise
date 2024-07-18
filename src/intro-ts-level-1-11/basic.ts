bf17056853d1ab7563801d758e304c227386aec9
TOezdemir <tolgay.oezdemir@hsrw.org>
2024-07-18 15:50:04 +0200

    type-script day 1

---
+++ src/intro-ts-level-1-11/basic.ts
@@ -0,0 +1,105 @@
+console.log("Welcome :)");
+
+let dogName :string  = "CHONKERBOIIIII";
+
+console.log(dogName);
+
+let istOnline: boolean = true
+let alter: number = 23
+let name: string = "Maximilian von Mustermannhausen"
+
+let istRegistriert: boolean = true
+let groesse: number = 1.70
+let beschreibung: string = "Das hier ist eine kurze eEschreibung des Täters. Männlich, 40, trägt eine auffällige Jacke mit einem Drachenaufnäher. Die Jacke ist weiß. Hüten SIE SICH VON DIESE MMANN!"
+console.log(beschreibung)
+
+let seventeen: number = 17;
+
+const sum = seventeen + 44;
+console.log(sum);
+
+
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+let x: number = 20
+let y: number = 30
+let z: number = 10
+let a: number = 15
+let b: number = 9
+let c: number = 20
+
+let addition: number = x + y
+console.log(addition)
+
+let subtraktion1 : number = y - x
+let subtraktion2 : number = x - y
+console.log(subtraktion1)
+console.log(subtraktion2)
+
+let multiplikation : number = x * y
+let division : number = x / y
+console.log(multiplikation)
+console.log(division)
+
+let resultOne : number = (x * y)  / z
+console.log(resultOne)
+
+let remainder : number = (a % b)
+console.log(remainder)
+
+let resultTwo : number = (a + b) * c
+console.log(resultTwo)
+
+a++;
+console.log(a);
+
+b --;
+console.log(b);
+
+const resultThree = b - a;
+console.log(resultThree);
+
+console.log(resultOne % resultTwo);
+
+let score: number = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;
+score = (5 + 5) * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;
+score += 10;  
+console.log('Ergebnis: ' + score);
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
